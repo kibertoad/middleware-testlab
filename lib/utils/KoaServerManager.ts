1f8fcbb337a101faf86588d1cf3e365ed15e9282
Igor Savin <iselwin@gmail.com>
2019-04-03 00:04:36 +0200

    First commit

---
+++ lib/utils/KoaServerManager.ts
@@ -0,0 +1,40 @@
+import { Server } from 'net'
+// @ts-ignore
+import Application from 'koa'
+
+export class KoaServerManager {
+  private server: Server
+
+  public startBeforeEach(app: Application, port: number = 8888): void {
+    beforeEach(() => {
+      this.stop()
+      this.server = app.listen(port)
+    })
+  }
+
+  public startBeforeAll(app: Application, port: number = 8888): void {
+    beforeEach(() => {
+      this.stop()
+      this.server = app.listen(port)
+    })
+  }
+
+  public stopAfterEach(): void {
+    afterEach(() => {
+      this.stop()
+    })
+  }
+
+  public stopAfterAll(): void {
+    afterAll(() => {
+      this.stop()
+    })
+  }
+
+  private stop(): void {
+    if (this.server) {
+      this.server.close()
+      this.server = undefined
+    }
+  }
+}
