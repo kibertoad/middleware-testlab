c8c5eb2f389bdec868f351c030e1a8b424f18f15
Igor Savin <iselwin@gmail.com>
2019-04-06 22:23:46 +0200

    Add missing exports

--- index-ts.ts
+++ index-ts.ts
@@ -1,6 +1,19 @@
 import { newKoaApp, DEFAULT_ENDPOINT } from './lib/apps/koaApp'
 import { newExpressApp } from './lib/apps/expressApp'
+
 import { KoaServerManagerJest } from './lib/utils/KoaServerManagerJest'
 import { KoaServerManagerMocha } from './lib/utils/KoaServerManagerMocha'
 
-export { newKoaApp, newExpressApp, KoaServerManagerJest, KoaServerManagerMocha, DEFAULT_ENDPOINT }
+import { okHandler as okHandlerKoa } from './lib/handlers/commonHandlersKoa'
+import { okHandler as okHandlerExpress } from './lib/handlers/commonHandlersExpress'
+
+const handlers = {
+  koa: {
+    okHandler: okHandlerKoa
+  },
+  express: {
+    okHandler: okHandlerExpress
+  }
+}
+
+export { handlers, newKoaApp, newExpressApp, KoaServerManagerJest, KoaServerManagerMocha, DEFAULT_ENDPOINT }
--- index.js
+++ index.js
@@ -1,8 +1,26 @@
 const { newKoaApp, DEFAULT_ENDPOINT } = require('./dist/lib/apps/koaApp')
 const { newExpressApp } = require('./dist/lib/apps/expressApp')
 
+const { KoaServerManagerJest } = require('./lib/utils/KoaServerManagerJest')
+const { KoaServerManagerMocha } = require('./lib/utils/KoaServerManagerMocha')
+
+const { okHandler: okHandlerKoa } = require('./lib/handlers/commonHandlersKoa')
+const { okHandler: okHandlerExpress } = require('./lib/handlers/commonHandlersExpress')
+
+const handlers = {
+  koa: {
+    okHandler: okHandlerKoa
+  },
+  express: {
+    okHandler: okHandlerExpress
+  }
+}
+
 module.exports = {
+  handlers,
   newExpressApp,
   newKoaApp,
+  KoaServerManagerJest,
+  KoaServerManagerMocha,
   DEFAULT_ENDPOINT
 }
